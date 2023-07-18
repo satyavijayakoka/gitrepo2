38fc4cd30e9d527b3e8e40639ce80cbd65ae1573
Madhu Duggirala <madhuduggirala@madhus-mbp.lan>
2023-07-18 19:02:04 -0400

    firstfeature

---
+++ python.py
@@ -0,0 +1,12 @@
+print("hello")
+print("hello")
+# git status - to track new files and changes over files
+# git init- initialise repo with git
+# git add . - all files will move to staging
+# git add javasample1.js - one file from working to staging
+# git commit -m "added"( first time configure email address and username)
+# go to github and create repository
+# add and make it public
+# git branch -M main - creating main branch
+# git remote add origin https://github.com/satyavijayakoka/gitpracticezorba17july- commit local repo to git remote repo
+# git push -u origin main - push the code and check repo
